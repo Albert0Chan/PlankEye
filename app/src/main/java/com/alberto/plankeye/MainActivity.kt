1fbc5b8a1b7c844112186aadc172b33d144331f3
Alberto <albertocssf@gmail.com>
2024-01-16 02:34:56 -0300

    Adicionando cor teste e botão

--- app/src/main/java/com/alberto/plankeye/MainActivity.kt
+++ app/src/main/java/com/alberto/plankeye/MainActivity.kt
@@ -1,13 +1,15 @@
-package com.alberto.plankeye
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            // Adicione a ação do botão aqui
+        }
     }
-
-
 }
