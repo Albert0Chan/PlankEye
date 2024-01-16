7e354e9bae899ddcf2138aa7b9f46df2d12f5636
Alberto <albertocssf@gmail.com>
2024-01-16 02:13:50 -0300

    Initial commit

---
+++ app/src/main/java/com/alberto/plankeye/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alberto.plankeye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
