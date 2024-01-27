64482cee18ff81e8376fd41dfe0106787d91ef24
Alberto <albertocssf@gmail.com>
2024-01-27 17:20:42 -0300

    LOGO - PlankEye

--- app/src/main/java/com/alberto/plankeye/MainActivity.kt
+++ app/src/main/java/com/alberto/plankeye/MainActivity.kt
@@ -1,6 +1,7 @@
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.alberto.plankeye.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
