84d164ee4f40022173befe654759f5a958aac36d
Alberto <albertocssf@gmail.com>
2024-01-27 18:22:15 -0300

    Segunda Tela

---
+++ app/src/main/java/com/alberto/plankeye/MainActivity2.kt
@@ -0,0 +1,16 @@
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.alberto.plankeye.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            // Adicione a ação do botão aqui
+        }
+    }
+}
